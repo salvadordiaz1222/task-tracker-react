71448063c6e1cf57a044e16b4c7410fbb7f3198d
salvadordiaz1222 <salvador.diaz1222@gmail.com>
2021-10-07 14:48:50 -0500

    Added useLocation to hide the Add button when we are in the about page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,31 @@
-import React from 'react'
-import Button from './Button'
-import PropTypes from 'prop-types'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Button from "./Button";
+import PropTypes from "prop-types";
 
 const Header = ({ title, onAdd, showAdd }) => {
-    
-    return (
-        <header className="header">
-            <h1>{title}</h1>
-            <Button 
-                color={showAdd ? "red" : "green"} 
-                text={showAdd ? "Close" : "Add"} 
-                // If `showAdd` is TRUE, then change the TEXT to `Close` else change the TEXT to `Add`
-                onClick={onAdd}
-            >
-            </Button>
-        </header>
-    )
-}
+  const location = useLocation();
+
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          // If `showAdd` is TRUE, then change the TEXT to `Close` else change the TEXT to `Add`
+          onClick={onAdd}
+        ></Button>
+      )}
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: "Task Tracker",
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
-export default Header
+  title: PropTypes.string.isRequired,
+};
+export default Header;
